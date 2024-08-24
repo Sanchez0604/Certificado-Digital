508c56b867b9ae083fd16b95e82147d130568cbe
Sanchez0604 <jpedro.c@outlook.com>
2024-08-23 21:54:06 -0300

    Criacao do usuario

--- API-Cert/src/main/java/com/jpedro/API_Cert/controller/UsuarioController.java
+++ API-Cert/src/main/java/com/jpedro/API_Cert/controller/UsuarioController.java
@@ -2,14 +2,17 @@ package com.jpedro.API_Cert.controller;
 
 import com.jpedro.API_Cert.models.Usuario;
 import com.jpedro.API_Cert.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
     @PostMapping
     public Usuario criar (@RequestBody Usuario usuario) {
-        return UsuarioRepository.save(usuario);
+        return usuarioRepository.save(usuario);
     }
 }
