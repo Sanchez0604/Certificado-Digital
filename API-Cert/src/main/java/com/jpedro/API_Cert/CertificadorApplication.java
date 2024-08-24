5f23bf32153fc1925540a1a923de0f256422286b
Sanchez0604 <jpedro.c@outlook.com>
2024-08-23 21:52:06 -0300

    Criacao do usuario

---
+++ API-Cert/src/main/java/com/jpedro/API_Cert/CertificadorApplication.java
@@ -0,0 +1,13 @@
+package com.jpedro.API_Cert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificadorApplication.class, args);
+	}
+
+}
---
+++ API-Cert/src/test/java/com/jpedro/API_Cert/CertificadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpedro.API_Cert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
